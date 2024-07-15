44a81b6ebd57e2dc98519f25bb6642fff4c9b8a4
Alexey Inkov <alex0982@mail.ru>
2024-07-15 23:09:24 +0300

    change fine name

--- src/parsertext.py
+++ src/parsertext.py
@@ -5,11 +5,26 @@ Created on Wed Nov 30 21:21:16 2022
 """
 
 import glob
+import re
 
 from loguru import logger
 
-from src.pdf_file import PDF_File
 from src.split import split_pdf_pages
+
+MONTHS = {
+    "январь": "01",
+    "февраль": "02",
+    "март": "03",
+    "апрель": "04",
+    "май": "05",
+    "июнь": "06",
+    "июль": "07",
+    "август": "08",
+    "сентябрь": "09",
+    "октябрь": "10",
+    "ноябрь": "11",
+    "декабрь": "12",
+}
 
 
 def find_file(dir_file, template):
@@ -24,95 +39,53 @@ def find_file(dir_file, template):
 
 
 def find_name(text):
-    months = {
-        "январь": "01",
-        "февраль": "02",
-        "март": "03",
-        "апрель": "04",
-        "май": "05",
-        "июнь": "06",
-        "июль": "07",
-        "август": "08",
-        "сентябрь": "09",
-        "октябрь": "10",
-        "ноябрь": "11",
-        "декабрь": "12",
-    }
-    lst = text.split()
-    print(lst)
-    try:
-        p = lst.index("за", 0, 30)
-        m = str(lst[p + 1]).lower()
-        try:
-            period = str(lst[p + 2]) + str(months[m])
-        except KeyError:
-            period = str(lst[p + 2]) + "XX"
-    except ValueError or UnboundLocalError:
-        p = 0
-        period = "По часовой 90"
-
-    #    print(period)
-    try:
-        an = lst.index("Адрес:", 0, 50)
-        try:
-            ak1 = lst.index("Телефон:", an)
-        except ValueError:
-            ak1 = 1000
-        try:
-            ak2 = lst.index("Строит.адрес:", an)
-        except ValueError:
-            ak2 = 1000
-        try:
-            ak3 = lst.index("Код", an)
-        except ValueError:
-            ak3 = 1000
-
-        if ak1 < ak2 and ak1 < ak3:
-            tso = "ТЭ"
-            ak = ak1
-        elif ak2 < ak3:
-            tso = "ТЭК"
-            ak = ak2
+    # print(text)
+    regex_address = r"(?<=[Адресол]{5}\W).{20,70}(?=[Номерc]{5})|(?<=[Адресол]{5}\W).{20,70}(?=[строитС]{6})|(?<=[Адресол]{5}\W).{20,70}(?=[Ккодл]{3})"
+    match_address = re.findall(regex_address, text, re.MULTILINE)
+    if match_address:
+        address = match_address[0]
+        logger.debug(f"Определил период: {address}")
+        regex_period = r"(?<=по\s\d{2}.)\d{2}\.\d{4}|\w{3,}\s\d{4}"
+        match_period = re.findall(regex_period, text, re.MULTILINE)
+        if match_period:
+            p = re.sub(r"\.", " ", match_period[0]).split()
+            logger.debug(f"Нашел дату: {p}")
+            if p[0].isdigit():
+                period = f"{p[-1]}{p[0]}"
+            else:
+                period = f"{p[-1]}{MONTHS.get(p[0].lower(), 'XX')}"
+            logger.debug(f"Определил период: {period}")
         else:
-            tso = "ПТЭ"
-            ak = ak3
-        print(f"(Find.Find_name): период {p}, адрес {an}-{ak}({ak1}, {ak2}, {ak3})")
-        if ak - an > 15:
-            newfilename = "Введи в ручную"
-        else:
-            adres = (
-                str(lst[an + 1])
-                .replace(".", "")
-                .replace(",", "")
-                .replace(":", "")
-                .replace("=", "")
-                .replace("?", "")
-                .replace("!", "")
-                .replace("|", "")
-                .replace('"', "")
-                .replace(">", "")
-                .replace("<", "")
-            )
-            for i in range(an + 2, ak):
-                adres += "_" + str(lst[i]).replace(".", "").replace(",", "").replace(
-                    ":", ""
-                ).replace("=", "").replace("?", "").replace("!", "").replace(
-                    "|", ""
-                ).replace(
-                    '"', ""
-                ).replace(
-                    ">", ""
-                ).replace(
-                    "<", ""
-                )
-            #    print(adres)
-            newfilename = f"{tso}_{period}_{adres[0:50]}.pdf"
-    except ValueError or UnboundLocalError:
-        newfilename = "Введи в ручную"
-    logger.info(f"Новое имя: {newfilename}")
-    return newfilename
+            logger.debug(f"Не распознал дату")
+            period = "XXXXXX"
 
+        filename = f"{period}_{address}"
+        filename = re.sub(r"\W", "_", filename)
+        filename = re.sub(r"_{2,}", "_", filename)
+        logger.debug(f"Новое имя файла: {filename}")
+        return f"{filename}.pdf"
+    else:
+        logger.debug(f"Не распознал адрес")
+        print(text)
+        raise ValueError
 
-# dir_file = 'C:/Users/Alex/Documents/Python/Project/pdf/2211'
-# template = 'pdf'
-# Find.Find_file(dir_file, template)
+    # for tso, regex in REGEX.items():
+    #     match = re.findall(regex, text, re.MULTILINE)
+    #     if len(match) == 2:
+    #         p = match[0].split(".")
+    #         logger.debug(f"Нашел дату: {p}")
+    #         if p[0].isdigit():
+    #             period = f"{p[-1]}{p[0]}"
+    #         else:
+    #             p = match[0].split()
+    #             period = f"{p[-1]}{MONTHS.get(p[0].lower(), 'XX')}"
+    #         logger.debug(f"Определил период: {period}")
+    #         address = match[1]
+    #         logger.debug(f"Определил адрес: {address}")
+    #         filename = f"{tso}_{period}_{address}"
+    #         filename = re.sub(r"\W", "_", filename)
+    #         filename = re.sub(r"_{2,}", "_", filename)
+    #         return f"{filename}.pdf"
+    # logger.debug(f"Не распознал")
+    # # print(text)
+    # raise ValueError
