1834bc482e0a346b4bff888354845e4aac1312a8
Alexey Inkov <122154128+AlexeyInkov@users.noreply.github.com>
2024-07-16 15:50:55 +0300

    Update config_data.py

--- src/config_data.py
+++ src/config_data.py
@@ -1,6 +1,8 @@
 class Settings:
     poppler_path = r"poppler-22.12.0\Library\bin"
     tesseract = r'C:\Program Files\Tesseract-OCR\tesseract.exe'
+    tesseract_lang = "rus"
+    tesseract_config = "--psm 4"
 
 
 settings = Settings()
